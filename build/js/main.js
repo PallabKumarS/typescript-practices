3a7db90e233e66fd1b1a4bfe407ec5f99c9de146
PallabKumarS <pallabkumar26@gmail.com>
2024-11-06 20:22:14 +0600

    first commit

---
+++ build/js/main.js
@@ -0,0 +1,12 @@
+"use strict";
+let a = 45;
+let b = 46;
+const test = (a, b) => {
+    if (a > b) {
+        console.log("A is bigger");
+    }
+    else {
+        console.log("B is bigger");
+    }
+};
+test(a, b);
---
+++ src/main.ts
@@ -0,0 +1,13 @@
+let a: number = 45;
+let b: number = 46;
+
+const test = (a: number, b: number) => {
+  if (a > b) {
+    console.log("A is bigger");
+  } else {
+    console.log("B is bigger");
+  }
+};
+
+// test(a, b);
+
